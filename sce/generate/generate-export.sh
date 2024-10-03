8f44a5a4a4c692b3932ee930ac5928d09d201196
Xaemiphor <xaemiphor@users.noreply.github.com>
2024-10-02 20:57:13 -0700

    fix: script was fuzzing too loosely against the CSV file

--- sce/generate/generate-export.sh
+++ sce/generate/generate-export.sh
@@ -24,9 +24,8 @@ genExcelTime() {
 	HOUR=$(echo $(( (${1} / 60 / 60) % 24 )))
 	echo "scale=8;${HOUR} / 24" | bc | sed 's/\.0*$//;s/\.\([0-9]*[1-9]\)0*$/.\1/'
 }
-
-CURRENT_TIME=${START_TIME}
-while [ ${CURRENT_TIME} -lt ${END_TIME} ]; do
+processTimestamp() {
+	CURRENT_TIME=${1}
 	NOW=$(date -u -d@${CURRENT_TIME} +'%Y-%m-%dT%H:00:00Z')
 	YEAR=$(date -u -d@${CURRENT_TIME} +%Y)
 	MONTH=$(date -u -d@${CURRENT_TIME} +%m)
@@ -35,12 +34,16 @@ while [ ${CURRENT_TIME} -lt ${END_TIME} ]; do
 
 	EXCEL_DATE=$(genExcelDate ${CURRENT_TIME})
 	EXCEL_TIME=$(genExcelTime ${CURRENT_TIME})
-	if [[ ${EXCEL_TIME} != "0" ]]; then
-		EXCEL_TIME="0${EXCEL_TIME}"
+	if [[ ${EXCEL_TIME} == "0" ]]; then
+		EXCEL_TIME="0"
+	elif [[ ${EXCEL_TIME:0:1} == "." ]]; then
+		EXCEL_TIME="0${EXCEL_TIME}[0-9]?"
 	fi
+	EXCEL_TIME=",${EXCEL_TIME},"
 	for RATE in ${RATES[@]}; do
-		if [[ ! -e "${DATA_DIR}/${RATE}.export/${YEAR}/${MONTH}/${DAY}/${HOUR}" || $(cat "${DATA_DIR}/${RATE}.export/${YEAR}/${MONTH}/${DAY}/${HOUR}") == "error" || $(cat "${DATA_DIR}/${RATE}.export/${YEAR}/${MONTH}/${DAY}/${HOUR}") =~ ^0[0-9]\. ]]; then
-			VALUES=( $(awk -F ',' -v kind=${RATE} -v edate=${EXCEL_DATE} -v etime=${EXCEL_TIME} '$0~kind && $0~edate && $0~etime{print $10}' "${SOURCE}") )
+		EXISTING=$(cat "${DATA_DIR}/${RATE}.export/${YEAR}/${MONTH}/${DAY}/${HOUR}" 2>/dev/null || true)
+		if [[ -z "${EXISTING}" || "${EXISTING}" == "error" || "${EXISTING}" =~ ^0[0-9]\. || "${EXISTING}" =~ ^[0-9] ]]; then
+			VALUES=( $(awk -F ',' -v kind=${RATE} -v edate=${EXCEL_DATE} -v etime=${EXCEL_TIME} '$2~kind && $3~edate && $0~etime{print $10}' "${SOURCE}") )
 			PRICE=0
 			for val in ${VALUES[@]}; do
 				PRICE=$(echo "${PRICE} + ${val}" | bc | sed 's/\.0*$//;s/\.\([0-9]*[1-9]\)0*$/.\1/')
@@ -60,5 +63,15 @@ while [ ${CURRENT_TIME} -lt ${END_TIME} ]; do
 
 		fi
 	done
+}
+
+num_jobs="\j"  # The prompt escape for number of jobs currently running
+num_procs=$(( $(nproc) - 1 ))
+CURRENT_TIME=${START_TIME}
+while [ ${CURRENT_TIME} -lt ${END_TIME} ]; do
+	while (( ${num_jobs@P} >= num_procs )); do
+                wait -n
+        done
+	processTimestamp "${CURRENT_TIME}" &
 	CURRENT_TIME=$(date -d@$(( ${CURRENT_TIME} + (60*60) )) +%s)
 done
